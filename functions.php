61d8e3d62c9be82dbd78cedba1fd394c19ddd6ea
MRpotman <camiloredondo5@gmail.com>
2026-02-06 17:21:24 -0600

    Creation of slider

--- functions.php
+++ functions.php
@@ -8,6 +8,8 @@ function mi_theme_assets() {
     [],
     '1.0'
   );
+   wp_enqueue_style(
+        'boskoa-hero', get_template_directory_uri() . '/assets/css/hero.css');
 }
 
 add_action('wp_enqueue_scripts', 'mi_theme_assets');
@@ -18,6 +20,14 @@ function boskoa_fonts() {
     'https://fonts.googleapis.com/css2?family=Architects+Daughter&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap',
     false
   );
+
+  wp_enqueue_style(
+        'boskoa-fontawesome',
+        'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+        array(),
+        '6.4.0'
+    );
+
 }
 add_action('wp_enqueue_scripts', 'boskoa_fonts');
 /******* */
