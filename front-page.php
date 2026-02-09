8e2f74f797e147e3281ec08935268eede69d7f22
MRpotman <camiloredondo5@gmail.com>
2026-02-08 20:34:32 -0600

    Info general de home

--- front-page.php
+++ front-page.php
@@ -66,12 +66,47 @@
   
 
   <section  class="general-info">
-    <div class="general-info-container">
-      
+    <?php
+      $infoPost = new WP_Query([
+        'post_type' => 'texto',
+        'name' => 'texto-costa-rica'
+      ]);
+    
+      if($infoPost -> have_posts()) :
+        while ($infoPost->have_posts()) : $infoPost->the_post();
+        $img = get_field('imagen');
+        $titulo = get_field('titulo');
+        $contenido = get_field('contenido');
+    ?>
 
+
+    <div class="general-info-costaRica">
+      <h2><?php echo esc_html($titulo); ?></h2>
+        <p><?php echo esc_html($contenido); ?></p>
+    </div>
+    <div class="general-info-minicard">
+
+        <div class="general-info-img"
+            style="background-image: url('<?php echo esc_url($img); ?>')">
+        </div>
+
+        <div class = "general-info-second">
+          <div class = "general-info-second-card">
+            <h2>+2 MILLIONS</h2>
+            <p>Many people fjdisofjiso fjndsofl paradise</p>
+          </div>
+          <button class="button">
+            <span class="button-content">Contact us >></span>
+          </button>
+        </div>
     </div>
 
-  
+    <?php
+        endwhile;
+        wp_reset_postdata();
+      endif;
+    ?>
+
   </section>
 
 </main>
