d97d92b39fdb3abf44c2da82e6f2e70a413b14e9
MRpotman <camiloredondo5@gmail.com>
2026-02-11 12:25:00 -0600

    los mismo refactorizado

--- about-sections/map-section.php
+++ about-sections/map-section.php
@@ -1,6 +1,6 @@
 <section class = "map-section-about-us">
 <div class = "map-about-us">
     <h2>We are here</h2>
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11091.913594771439!2d-85.44254763425313!3d10.61168836434106!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f757cd452c44b1f%3A0xd9112effc575f1b9!2sMulticines%20Liberia!5e0!3m2!1ses!2scr!4v1770832799466!5m2!1ses!2scr" width="1000" height="800" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11091.913594771439!2d-85.44254763425313!3d10.61168836434106!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f757cd452c44b1f%3A0xd9112effc575f1b9!2sMulticines%20Liberia!5e0!3m2!1ses!2scr!4v1770832799466!5m2!1ses!2scr" width="1000" height="500" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 </div>
 </section>
