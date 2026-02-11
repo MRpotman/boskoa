e29857ff24e43fd54a3c24f6528b66b891469c64
MRpotman <camiloredondo5@gmail.com>
2026-02-11 12:12:15 -0600

    insert of the map on about-us section

--- about.php
+++ about.php
@@ -16,6 +16,9 @@ get_template_part('about-sections/about-sections');
 // Sección de Equipo
 get_template_part('about-sections/about-team');
 
+// sección de mapa
+get_template_part('about-sections/map-section');
+
 // Sección de Reservas
 get_template_part('about-sections/about-reservation');
 
